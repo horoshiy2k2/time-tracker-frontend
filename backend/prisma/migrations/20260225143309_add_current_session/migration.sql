241a2a7b4ae591bd8e0d9b56f359a351fa1e418c
Anton Kharuzhy <antonio.horoshiy@gmail.com>
2026-03-17 12:57:44 +0300

    Delete backend directory

--- backend/prisma/migrations/20260225143309_add_current_session/migration.sql
+++
@@ -1,8 +0,0 @@
--- CreateTable
-CREATE TABLE "CurrentSession" (
-    "id" TEXT NOT NULL PRIMARY KEY,
-    "categoryId" TEXT,
-    "startTime" DATETIME NOT NULL,
-    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    CONSTRAINT "CurrentSession_categoryId_fkey" FOREIGN KEY ("categoryId") REFERENCES "Category" ("id") ON DELETE SET NULL ON UPDATE CASCADE
-);
